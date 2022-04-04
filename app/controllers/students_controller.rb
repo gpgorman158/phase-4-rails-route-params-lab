756c5d39a81e8d0f3b6b48f9bc2a82ecd65fa503
Gerald Gorman <gpgorman158@gmail.com>
2022-04-04 17:13:19 -0400

    commit tree 7fdce6641d82d34a6cb6f2086d53a6f4197fbd50

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+    student = Student.find_by(first_name: params[:firstName], last_name: params[:lastName])
+    render json: student
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
